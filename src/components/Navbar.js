2003537b12f6dfe938154acea9d1989368bf5678
Prabhu Kumar <prabhukumarsawraj@gmail.com>
2023-05-14 03:19:14 +0530

    kk

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
 
-const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+const { loginWithRedirect, logout, user } = useAuth0();
       
 
   return (
